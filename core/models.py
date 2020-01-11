6822dbba6372ebf0a6b525f82b7b72c77533bf32
Remus-Antonio <ekro98@gmail.com>
2020-01-11 10:56:49 +0200

    Added Business and review models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class MyUser(User):
+    pass
+
+
+class Business(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='businesses')
+    image = models.ImageField(upload_to='images/business')
+    stars = models.PositiveIntegerField()
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=4096)
+
+
+class Review(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
